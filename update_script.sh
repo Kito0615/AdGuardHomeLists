769c62bd819800d65a8d5af3b4440d472de1efdb
Anar <anar0615@outlook.com>
2024-04-11 18:07:36 +0800

    Update At 1712830056

--- update_script.sh
+++ update_script.sh
@@ -2,3 +2,7 @@ curl -o temp.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.tx
 curl -o temp.txt https://anti-ad.net/adguard.txt && mv temp.txt ant-ad-easylist.txt
 curl -o temp.txt https://raw.githubusercontent.com/sbwml/halflife-list/master/ad.txt && mv temp.txt Halflife-List.txt
 curl -o temp.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt && mv temp.txt xinggsf.txt
+git add .
+timestamp=$(date +%s)
+git commit -m "Update At $timestamp"
+git pull
