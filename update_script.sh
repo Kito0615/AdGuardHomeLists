e2d492c4e5394dd4e1dca25682d06b5f677c0480
Anar <anar0615@outlook.com>
2024-04-11 17:44:48 +0800

    latest update

---
+++ update_script.sh
@@ -0,0 +1,4 @@
+curl -o temp.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt && mv temp.txt AdBlockPlus_2_0.txt
+curl -o temp.txt https://anti-ad.net/adguard.txt && mv temp.txt ant-ad-easylist.txt
+curl -o temp.txt https://raw.githubusercontent.com/sbwml/halflife-list/master/ad.txt && mv temp.txt Halflife-List.txt
+curl -o temp.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt && mv temp.txt xinggsf.txt
