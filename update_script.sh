792233d42cc89a51cbb4f06ebe21f76726aa4fb4
Anar <anar0615@outlook.com>
2024-04-17 18:55:15 +0800

    Update At 202404171855

--- update_script.sh
+++ update_script.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+cd  /Users/digiarty/Documents/personal/Documents/AdGuardHomeLists
 curl -o temp.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/dns.txt && mv temp.txt AdBlockPlus_2_0.txt
 curl -o temp.txt https://anti-ad.net/adguard.txt && mv temp.txt ant-ad-easylist.txt
 curl -o temp.txt https://raw.githubusercontent.com/sbwml/halflife-list/master/ad.txt && mv temp.txt Halflife-List.txt
 curl -o temp.txt https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt && mv temp.txt xinggsf.txt
+
 git add .
 timestamp=$(date +"%Y%m%d%H%M")
 git commit -m "Update At $timestamp"
